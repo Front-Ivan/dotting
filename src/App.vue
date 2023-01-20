d1c71813e301e464fb9049673d66cffec78dc351
Egor-Babashko <egor.babashko2005@mail.ru>
2023-01-20 16:31:48 +0300

    add Drag'nDrop

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,20 @@
 <template>
   <div>
     <!--    <div style="margin-top: 1vh;"></div>-->
-    <div class="content" style="width: fit-content; " @click="setPoint($event)">
+    <div
+        class="content"
+        style="width: fit-content; "
+        @mousemove="dragnDropInProcess($event)"
+        @mouseleave="clicked = false"
+        @mouseup="clicked = false"
+        @click="setPoint($event)"
+    >
       <img src="@/assets/images/building.jpg" alt="" ref="img" @load="imgWidth = $refs.img.width">
       <div
           class="circle"
+          data-circle
           v-for="(point, idx) in points"
+          ref="circle"
           :key="idx"
           :style="`
           width: calc(10px / ${(point.installationWidth / imgWidth)});
@@ -13,6 +22,8 @@
           top: ${(point.y - 4.25) / (point.installationWidth / imgWidth)}px;
           left: ${(point.x - 4.25) / (point.installationWidth / imgWidth)}px;
           `"
+          @mousedown.prevent="dragnDropStart($event, idx)"
+          @mouseup.prevent="dragnDropEnd($event)"
       />
     </div>
   </div>
@@ -25,20 +36,21 @@ export default {
   components: {},
   data() {
     return {
-      points: [{"x": 252, "y": 207, "installationWidth": 843}, {"x": 484, "y": 58, "installationWidth": 843}, {
-        "x": 651,
-        "y": 175,
-        "installationWidth": 843
-      }, {"x": 593, "y": 135, "installationWidth": 843}, {"x": 553, "y": 115, "installationWidth": 843}, {
-        "x": 609,
-        "y": 156,
-        "installationWidth": 843
-      }, {"x": 370, "y": 111, "installationWidth": 843}, {"x": 359, "y": 125, "installationWidth": 843}, {
-        "x": 311,
-        "y": 153,
-        "installationWidth": 843
-      }, {"x": 304, "y": 181, "installationWidth": 843}],
+      points: [
+        {"x": 252, "y": 207, "installationWidth": 843},
+        {"x": 484, "y": 58, "installationWidth": 843},
+        {"x": 651, "y": 175, "installationWidth": 843},
+        {"x": 593, "y": 135, "installationWidth": 843},
+        {"x": 553, "y": 115, "installationWidth": 843},
+        {"x": 609, "y": 156, "installationWidth": 843},
+        {"x": 370, "y": 111, "installationWidth": 843},
+        {"x": 359, "y": 125, "installationWidth": 843},
+        {"x": 311, "y": 153, "installationWidth": 843},
+        {"x": 304, "y": 181, "installationWidth": 843}
+      ],
       imgWidth: 0,
+      clicked: false,
+      selectedPoint: null,
     }
   },
   methods: {
@@ -46,7 +58,33 @@ export default {
       console.log('X', e.layerX)
       console.log('Y', e.layerY)
       console.log(e)
-      this.points.push({x: e.layerX, y: e.layerY, installationWidth: this.$refs.img.width})
+      this.points.push({x: this.$refs.img.width - 10, y: this.$refs.img.height - 10, installationWidth: this.$refs.img.width})
+    },
+    dragnDropStart(e, idx) {
+      // console.log(e)
+      this.clicked = true
+      this.selectedPoint = idx
+    },
+    dragnDropInProcess(e) {
+      if (this.clicked) {
+        const widthHeight = e.target.offsetWidth
+        if (this.$refs.img.width > Math.abs(this.points[this.selectedPoint].x)) {
+          this.points[this.selectedPoint].x = this.points[this.selectedPoint].x + e.movementX
+        }
+        if (this.$refs.img.height > Math.abs(this.points[this.selectedPoint].y)) {
+          this.points[this.selectedPoint].y = this.points[this.selectedPoint].y + e.movementY
+        }
+        // console.log('point', this.points[this.selectedPoint])
+        // console.log('event', e)
+      }
+    },
+    dragnDropEnd(e) {
+      // console.log(e)
+      this.clicked = false
+      this.selectedPoint = null
+    },
+    checkPoint(e) {
+      console.log(e.target)
     }
   },
   computed: {
@@ -57,8 +95,12 @@ export default {
       return window.innerWidth / 100
     },
   },
+  watch: {
+
+  },
   mounted() {
-    console.log()
+  },
+  updated() {
   }
 }
 </script>
